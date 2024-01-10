c5b33e1bc71bfee3f98f0b27ef07e5f58cc15195
Dmitriy Kovalev <rouxx26@gmail.com>
2024-01-10 18:21:25 +0300

    init

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Theme entry point
+ */
+if (!defined('ABSPATH')) exit;
+
+//require get_template_directory() . '/inc/helpers/index.php'; // All utilities
+//require get_template_directory() . '/inc/setup.php'; // Theme setup
+//require get_template_directory() . '/inc/enqueue.php'; // Scripts and styles enqueue
+//require get_template_directory() . '/inc/actions.php'; // Hooks
